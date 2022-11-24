ba417c556b6e9f59fb2d18a496f44dcc9d3b701e
4c656f <85838594+4c656f@users.noreply.github.com>
2022-11-24 22:45:28 +0600

    fix imports (#1)
    * fix imports
    
    * fix typo
    
    * fix some typo #2

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,32 +1,41 @@
 /* eslint-disable no-console */
-import { component$, Resource, useResource$ } from '@builder.io/qwik';
-import { Framework } from '@prisma/client';
-import { tServer } from '~/server/router';
+import {component$, Resource, useResource$} from '@builder.io/qwik';
+import {isServer} from "@builder.io/qwik/build";
+
 
 export default component$(() => {
-	const itemsResource = useResource$<Framework[]>(() => getFrameworks());
 
-	return (
-		<Resource
-			value={itemsResource}
-			onPending={() => <>Loading...</>}
-			onRejected={(error) => <>Error: {error.message}</>}
-			onResolved={(items: Framework[]) => (
-				<div>
-					Records:
-					{items.map((item) => (
-						<>
-							<div>Id: {item.id}</div>
-							<div>Name: {item.name}</div>
-							<hr />
-						</>
-					))}
-				</div>
-			)}
-		/>
-	);
+
+    const itemsResource = useResource$(async () => {
+        if (isServer) {
+            const {tServer} = await import('~/server/router')
+            return tServer.framework.list('')
+        }
+        const {trpc} = await import('../client/trpc')
+        return trpc.framework.list.query('')
+    });
+
+    return (
+
+        <Resource
+            value={itemsResource}
+            onPending={() => <>Loading...</>}
+            onRejected={(error) => <>Error: {error.message}</>}
+            onResolved={(items) => (
+                <div>
+                    Records:
+                    {items.map((item) => (
+                        <>
+                            <div>Id: {item.id}</div>
+                            <div>Name: {item.name}</div>
+                            <hr />
+                        </>
+                    ))}
+                </div>
+            )}
+        />
+
+    );
 });
 
-export async function getFrameworks(): Promise<Framework[]> {
-	return await tServer.framework.list('');
-}
+
