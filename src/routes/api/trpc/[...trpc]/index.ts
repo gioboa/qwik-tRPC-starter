cf59dece69579fb144dd90c20dd9afe5d5e10321
gioboa <giorgiob.boa@gmail.com>
2023-12-06 12:47:08 +0100

    chore: update project

---
+++ src/routes/api/trpc/[...trpc]/index.ts
@@ -0,0 +1,25 @@
+import { RequestHandler } from '@builder.io/qwik-city';
+import { HTTPHeaders } from '@trpc/server/dist/http/internals/types';
+import { resolveHTTPResponse } from '@trpc/server/http';
+import { createContext } from '../../../../server/context';
+import { appRouter } from '../../../../server/router/index';
+
+export const onRequest: RequestHandler = async ({ request, params, json }) => {
+	try {
+		console.log('-----');
+		const httpResponse = await resolveHTTPResponse({
+			router: appRouter,
+			path: params.trpc,
+			req: {
+				body: await request.text(),
+				headers: request.headers as unknown as HTTPHeaders,
+				method: request.method,
+				query: new URL(request.url).searchParams,
+			},
+			createContext,
+		});
+		json(200, JSON.parse(httpResponse.body || '{}'));
+	} catch (error: any) {
+		throw error(500, 'ERROR: Internal Server Error');
+	}
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@
 import { component$ } from '@builder.io/qwik';
 import { routeLoader$ } from '@builder.io/qwik-city';
 import { tServer } from '~/server/router';
+import { trpc } from '../client/trpc';
 
 export const useFrameworks = routeLoader$(async () => {
 	return await tServer.framework.list('');
@@ -12,14 +13,23 @@ export default component$(() => {
 
 	return (
 		<div>
-			Records:
-			{frameworksSig.value.map((framework) => (
-				<>
-					<div>Id: {framework.id}</div>
-					<div>Name: {framework.name}</div>
-					<hr />
-				</>
-			))}
+			<button
+				onClick$={async () => {
+					console.log(await trpc.framework.list.query(''));
+				}}
+			>
+				tRPC Client
+			</button>
+			<div>
+				Records:
+				{frameworksSig.value.map((framework) => (
+					<>
+						<div>Id: {framework.id}</div>
+						<div>Name: {framework.name}</div>
+						<hr />
+					</>
+				))}
+			</div>
 		</div>
 	);
 });
